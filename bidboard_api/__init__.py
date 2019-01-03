1767e3bc0b499ec2b34a69620a6ef240fc9eb85e
Ahmed Ramzi <ahmedramzy160@yahoo.com>
2019-01-03 12:25:45 +0800

    built new apis for billboards and bids

--- bidboard_api/__init__.py
+++ bidboard_api/__init__.py
@@ -7,7 +7,11 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 from bidboard_api.blueprints.media.views import media_api_blueprint
 from bidboard_api.blueprints.users.views import users_api_blueprint
 from bidboard_api.blueprints.sessions.views import sessions_api_blueprint
+from bidboard_api.blueprints.billboards.views import billboards_api_blueprint
+from bidboard_api.blueprints.bids.views import bids_api_blueprint
 
 app.register_blueprint(sessions_api_blueprint, url_prefix='/api/v1/')
 app.register_blueprint(media_api_blueprint, url_prefix='/api/v1/media')
 app.register_blueprint(users_api_blueprint, url_prefix='/api/v1/users')
+app.register_blueprint(billboards_api_blueprint, url_prefix='/api/v1/billboards')
+app.register_blueprint(bids_api_blueprint, url_prefix='/api/v1/bids')
